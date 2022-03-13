2a8eb891496617ded45ad2bca46e66d3f76fe40c
tohalike <thetoxalike444@gmail.com>
2022-03-13 19:52:40 +0300

    load

---
+++ app.js
@@ -0,0 +1,29 @@
+// const attetion = prompt('Первый раз на сайте?');
+
+// if(attetion !== 'да' || 'Да') {
+//     alert('Добро пожаловать!');
+// } else if(attetion !== 'нет' || 'Нет') {
+//     alert('С возвращением!');
+// }
+
+
+
+// const arr = [55, 32, 54, 23, 77];
+// let q;
+
+// for(let i = 0; i <= arr.length - 1; i++){
+//     for(let j = 0; j <= arr.length - i - 1; j++){
+//         if(arr[j] >= arr[j + 1]){
+//             q = arr[j];
+//             arr[j] = arr[j + 1];
+//             arr[j + 1] = q;
+//         }
+//     }
+// }
+
+// console.log(arr)
+
+// function myButton(){
+//     let btn = document.getElementById('btn').innerHTML = alert('Hello')
+
+// }
